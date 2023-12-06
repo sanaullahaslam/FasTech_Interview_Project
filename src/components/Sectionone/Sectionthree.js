ae320180358448d0d6d8face366faf6b5d0f689b
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-12-07 01:08:02 +0500

    component

--- src/components/Sectionone/Sectionthree.js
+++ src/components/Sectionone/Sectionthree.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import img2 from '../../assets/img2.PNG'
+import img2 from '../../assets/img2.PNG';
+import data from '../../assets/data.PNG';
+import technology from '../../assets/technology.PNG';
+import team from '../../assets/team.PNG';
 
-const ResponsiveImageSection = () => {
+const ImageSection = () => {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      {/* Centered Heading Image */}
       <img
-        src={img2}// Replace with the actual URL or import the image
-        alt="Your Alt Text"
-        style={{ width: '100%', height: 'auto', borderRadius: '10px' }}
+        src={img2}
+        alt="Centered Image Alt Text"
+        style={{
+          width: '100%',
+          maxWidth: '800px',
+          height: 'auto',
+          display: 'block',
+          margin: '0 auto',
+          borderRadius: '10px',
+        }}
       />
+
+      {/* Three Images in Series */}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          marginTop: '20px',
+          flexWrap: 'wrap', // Allow images to wrap on smaller screens
+        }}
+      >
+        {/* Image 1 */}
+        <img
+          src={team}
+          alt="Image 1 Alt Text"
+          style={{ width: '100%', maxWidth: '300px', borderRadius: '10px', marginBottom: '20px' }}
+        />
+
+        {/* Image 2 */}
+        <img
+          src={technology}
+          alt="Image 2 Alt Text"
+          style={{ width: '100%', maxWidth: '300px', borderRadius: '10px', marginBottom: '20px' }}
+        />
+
+        {/* Image 3 */}
+        <img
+          src={data}
+          alt="Image 3 Alt Text"
+          style={{ width: '100%', maxWidth: '300px', borderRadius: '10px', marginBottom: '20px' }}
+        />
+      </div>
     </div>
   );
 };
 
-export default ResponsiveImageSection;
+export default ImageSection;
