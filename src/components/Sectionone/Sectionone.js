e3c976f7a4d4f1fec0402cbc2a6ff19ce9c1d3df
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-12-09 22:39:16 +0500

    2

--- src/components/Sectionone/Sectionone.js
+++ src/components/Sectionone/Sectionone.js
@@ -9,7 +9,6 @@ const PortfolioSection = () => {
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
       </p>
-     
       <button
         style={{
           backgroundColor: '#A73A3A',
